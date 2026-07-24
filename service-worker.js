8b5087143ac19736359b2e4651c68f90df69135e
Talha Kıyak <talha.kiyak@aurorabilisim.com>
2026-07-25 00:25:22 +0300

    İlk sürüm: PWA Sprint Planlayıcı ve Efor Tahmin

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'planner-cache-v1';
+const urlsToCache = [
+  './',
+  './planlayici.html',
+  './estimation.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
